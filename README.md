f0df8217e814d98b63d4999bf0db24373a411a5e
Ben Cotton <bcotton@funnelfiasco.com>
2016-04-15 00:46:13 -0400

    A first pass at a script that will do what I need. Hopefully it does what other people need, too.

---
+++ README.md
@@ -0,0 +1,19 @@
+# mtppics.sh  
+
+*Copyright 2016 by Ben Cotton* 
+
+*Licensed under the GNU General Public License version 3 (or later). See COPYING.txt for full terms.*
+
+This script is designed to copy pictures off of Android phones to some location on your hard drive.
+The KDE MTP stack and Samsung phones don't get along very well, so I wrote this to make it easy for
+my wife and I to save off our pictures on a regular basis. The general idea is that you're doing it
+on a time-based system (months for now).
+
+## Options
+
+**-l** Use last month as the date
+**-s *string* ** Use *string* as the date (it must appease date(1)).
+
+## Requirements
+
+* `simple-mtpfs`
---
+++ mtppics.sh
@@ -0,0 +1,89 @@
+#!/bin/bash
+
+####################
+# mtppics.sh
+# Copyright 2016 by Ben Cotton
+# License: GNU GPLv3+ 
+#
+# This script is mostly just a piece of garbage to help my wife and I copy
+# pictures off our phones on a regular basis. It's probably not going to work
+# very well for you, but maybe it will someday...
+####################
+
+# By default, where are we copying the files from? Should be a relative path on
+# the MTP device.
+SOURCE_DIR=${MTPPICS_SOURCE_DIR:-"Card/DCIM/Camera"}
+# By default, where are we copying the files to? Can be an absolute or relative
+# path on the computer. You can use date(1) format variables in here.
+DEST_DIR=${MTPPICS_DEST_DIR:-"Pictures/%Y/%m"}
+
+_VERSION='0.1'
+_COPYRIGHT="Copyright 2016 - Ben Cotton"
+
+function _cleanup {
+    # Clean up after ourselves
+    if [ "${MTP_MOUNT}" ]; then
+        echo "Unmounting ${MTP_MOUNT}"
+        fusermount -u ${MTP_MOUNT} || \
+            echo "WARNING: Could not unmount ${MTP_MOUNT}"
+        rmdir ${MTP_MOUNT} || echo "WARNING: Could not remove ${MTP_MOUNT}"
+    fi
+    
+    read -p 'Press the enter key to exit'
+    exit
+}
+
+function _errorhandler {
+    echo "Encountered an error ${1}"
+    echo 'Aborting!'
+    _cleanup
+}
+
+trap '_errorhandler "${EMSG:-"Unknown error"}"' ERR SIGINT SIGTERM SIGHUP
+
+echo "mtppics.sh - v${_VERSION} - ${_COPYRIGHT}"
+
+# We're going to get pretty crazy with the date(1) command.
+CURRENT_DATE='now'
+while getopts ":ls:" option; do
+    case $option in
+        l)
+            # Archive pictures for the last month.
+            CURRENT_DATE="$(date +%Y-%m-15) -1 month"
+            ;;
+        s)
+            # Archive pictures for a given date string.
+            CURRENT_DATE="${OPTARG}"
+            ;;
+        \?)
+            _errorhandler "unknown option -$OPTARG"
+            ;;
+        :)
+            _errorhandler "missing argument to -$OPTARG"
+            ;;
+    esac
+done
+
+# We use the EMSG to give a user-helpful message about what we we're doing and
+# also to double as a comment on what...we're doing.
+EMSG='making temporary directory'
+MTP_MOUNT=$(mktemp -d)
+EMSG='looking for simple-mtpfs command'
+MTPFSCMD=$(which simple-mtpfs)
+
+EMSG='mounting MTP device'
+${MTPFSCMD} ${MTP_MOUNT}
+
+YYYYMM=$(date --date="${CURRENT_DATE}" +%Y%m)
+DEST_DIR=$(date --date="${CURRENT_DATE}" "+${DEST_DIR}")
+
+EMSG='making sure destination directory exists'
+mkdir -p "${DEST_DIR}"
+
+EMSG="copying files to ${DEST_DIR}"
+echo "Ready to start ${EMSG}"
+rsync -v "${MTP_MOUNT}/${SOURCE_DIR}/"*${YYYYMM}* "${DEST_DIR}"
+
+echo 'Copy complete'
+
+_cleanup
